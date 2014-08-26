3d7f61adb6914be775ca222ee7588973fecfe03c
Philip Nuzhnyy <philip.nuzhnyy@gmail.com>
2014-08-26 16:34:42 +0200

    normalize url protocol to be http

--- extension/src/bg/background.js
+++ extension/src/bg/background.js
@@ -58,8 +58,21 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
                 return;
             }
 
+            function normalizeUrlProtocol(url){
+                // move everything to http
+                if(url.indexOf("https://") !== -1){
+                    url = url.replace("https://","http://");
+                }
+
+                if(url.indexOf("http://") === -1){
+                    url = ["http://",url].join('');
+                }
+
+                return url;
+            }
+
             var spotting = { 
-                url : message.from, 
+                url : normalizeUrlProtocol(message.from),
                 meta : message.meta,
                 spottedBy : Identity.getUserId()
             };
